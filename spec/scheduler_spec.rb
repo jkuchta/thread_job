fdf2d69c6c953c8ccf25dda69959c4055385101b
Jeff Kuchta <jkuchta@gmail.com>
2016-06-21 22:49:05 -0700

    Added some basic tests to scheduler

--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -10,17 +10,43 @@ describe ThreadJob::Scheduler do
   end
 
   describe '#add_job' do
-    it 'does not raise an expection' do
-      scheduler = ThreadJob::Scheduler.new("default")
+    context 'scheduler has not started' do
+      let(:scheduler) { ThreadJob::Scheduler.new('default') }
 
-      class ExJob < ThreadJob::Job
-        def run
-          puts "hello"
+      it 'adds a job to the job_store' do
+        class ExJob < ThreadJob::Job
+          def run
+            puts "hello"
+          end
         end
-      end
 
-      job = ExJob.new
-      scheduler.add_job("Example Job", job)
+        job = ExJob.new
+        scheduler.add_job("Example Job", job)
+        store = scheduler.instance_variable_get(:@job_store)
+        job_queue = store.instance_variable_get(:@jobs)['default']
+        expect(job_queue.length).to equal(1)
+      end
+    end
+
+    # Use zero workers so that items are not being removed from queue in test
+    context 'scheduler has started but has no workers in thread pool' do
+      let(:scheduler) { ThreadJob::Scheduler.new('default', ThreadJob::Memory::Store.new, 2, 0) }
+      before { scheduler.start }
+
+      it 'adds a job to the job_store' do
+        class ExJob < ThreadJob::Job
+          def run
+            puts "hello"
+          end
+        end
+
+        job = ExJob.new
+        scheduler.add_job("Example Job", job)
+        scheduler.add_job("Example Job 2", job)
+        store = scheduler.instance_variable_get(:@job_store)
+        job_queue = store.instance_variable_get(:@jobs)['default']
+        expect(job_queue.length).to equal(2)
+      end
     end
   end
 
